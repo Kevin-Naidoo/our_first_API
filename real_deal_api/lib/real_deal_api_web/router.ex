5e31e02eadc6210086f9d57a5e8b627a8fdc37df
Kevin-Naidoo <kevinnelsonnaidoo@gmail.com>
2023-08-17 10:04:09 +0200

    7. Protected Endpoints for Elixir REST API Project

--- real_deal_api/lib/real_deal_api_web/router.ex
+++ real_deal_api/lib/real_deal_api_web/router.ex
@@ -1,27 +1,34 @@
 defmodule RealDealApiWeb.Router do
-  #alias RealDealApiWeb.AccountController
-  #alias RealDealApiWeb.AccountControllerTest
-  #alias RealDealApiWeb.DefaultController
+
   use RealDealApiWeb, :router
   use Plug.ErrorHandler
 
-  defp handle_errors(conn, %{reason: %Phoenix.Router.NoRouteError{message: message}}) do
+  def handle_errors(conn, %{reason: %Phoenix.Router.NoRouteError{message: message}}) do
     conn |> json(%{errors: message}) |> halt()
   end
 
-  defp handle_errors(conn, %{reason: %{message: message}}) do
+  def handle_errors(conn, %{reason: %{message: message}}) do
     conn |> json(%{errors: message}) |> halt()
   end
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :auth do
+    plug RealDealApiWeb.Auth.Pipeline
   end
 
   scope "/api", RealDealApiWeb do
     pipe_through :api
     get "/", DefaultController, :index
-    #resources "/accounts", AccountController, except: [:new, :edit]
     post "/accounts/create", AccountController, :create
     post "/accounts/sign_in", AccountController, :sign_in
-
   end
+
+  scope "/api", RealDealApiWeb do
+    pipe_through [:api, :auth]
+    get "/accounts/by_id/:id", AccountController, :show
+  end
+
 end
